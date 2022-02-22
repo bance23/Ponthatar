b99663d610453e962652e64ac3daf82d11e04cd2
Bence Benedek Balogh <2021290@dszcberegszaszi.hu>
2022-02-22 12:09:33 +0100

    update

--- hianyzas/Program.cs
+++ hianyzas/Program.cs
@@ -10,14 +10,32 @@ namespace hianyzas
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hány hiányzása van?");
             int hianyzas = Convert.ToInt32(Console.ReadLine());
 
-            if (hianyzas > 10)
+            if (hianyzas > 9)
             {
                 Console.WriteLine("Add meg a születési dátumod:");
                 string szuldat = Console.ReadLine();
                 Console.WriteLine("Ekkor született:{0}", szuldat, "és ennyit hiányzott:{0}", hianyzas);
             }
+            if (9 < hianyzas & hianyzas < 16)
+            {
+                Console.WriteLine("Figyelmeztetés");
+            }
+            else if (15 < hianyzas & hianyzas < 20)
+            {
+                Console.WriteLine("Osztályfőnöki megrovó");
+            }
+            else if (19 < hianyzas & hianyzas < 30)
+            {
+                Console.WriteLine("Igazgatói megrovó");
+            }
+            else
+            {
+                Console.WriteLine("Fegyelmi");
+            }
+            Console.ReadKey();
         }
     }
 }
