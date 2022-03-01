eaa87f393bc29e1f0dfcfef46191263d383239b2
Bence Benedek Balogh <2021290@dszcberegszaszi.hu>
2022-03-01 11:38:24 +0100

    tanárüzenikapcsoldkiagéped

---
+++ osztaly/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osztaly
+{
+    class Allat
+    {
+        private string nev;
+        private string fajta;
+        private string szin;
+        private int kor;
+
+        public Allat(string nev, string fajta, string szin)
+        {
+            this.nev = nev;
+            this.fajta = fajta;
+            this.szin = szin;
+        }
+
+        public string getNevrz
+            sáfuűnsékrtuűmért
+            upsfg
+            jég()
+        {
+            return nev;
+        }
+        public int getKor()
+        {
+            return kor;
+        }
+        public string getFajta()
+        {
+            return fajta;
+        }
+        public void setNev(string nev)
+        {
+            this.nev = nev;
+        }szntszuh
+            stusrtlu
+            űúdsku
+            dsfáujdűtziu
+            dézj
+        public void setKor(int kor)
+        {
+            this.kor = kor;
+        }
+        public void setFajta(string fajta)
+        {
+            this.fajta = fajta;hpksoudfhélkjsdfhléksdfhkjnsdfűh
+                dsyúfhstm
+                s
+                jhásrtu
+                ús
+        }
+
+        Máskor kapcsold ki a géped jóember!!!
+
+            https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi-xeP126T2AhWEjaQKHTCeDvkQFnoECAYQAQ&url=https%3A%2F%2Findex.hu%2Fkulfold%2F2022%2F03%2F01%2Foroszorszag-ukrajna-orosz-ukran-haboru-konfliktus-vlagyimir-putyin-volodimir-zelenszkij-tamadas%2F&usg=AOvVaw3TeVbPHOIy5R7SRKA482Kk
+            https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi-xeP126T2AhWEjaQKHTCeDvkQvOMEKAB6BAgCEAE&url=https%3A%2F%2Fwww.portfolio.hu%2Fglobal%2F20220301%2Fbrutalis-sereg-indult-kijev-elfoglalasara-a-haboru-eddigi-legnehezebb-orai-elott-allunk-529903&usg=AOvVaw2pyXpv-ahuS_2M72-VN4UV
+            https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi-xeP126T2AhWEjaQKHTCeDvkQFnoECCMQAQ&url=https%3A%2F%2Fhvg.hu%2Fcs%2Forosz-ukr%25C3%25A1n%2520konfliktus&usg=AOvVaw0O-iyQXCB9DR5VKxI7J8Cz
+            https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiyx4yD3KT2AhUF_6QKHf3ECzwQvOMEKAB6BAgCEAE&url=https%3A%2F%2Findex.hu%2Fbelfold%2F2022%2F03%2F01%2Forban-viktor-nagykovetek-ertekezlet-orosz-ukran-haboru-szijjarto%2F&usg=AOvVaw2ACQ0xBAWEIMY_jp7PKiHj
+            Ésdlfkjptga
+            drélujnsadph
+            dfpzhju
+            sdflzjsf
+            gzjhs
+            jh
+            főljh
+            sasfhéjshjséhlksf
+            gh
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Allat allat = new Allat("Bodri", "keverék", "barna");
+            allat.setKor(2);
+            Console.WriteLine(allat.getKor());
+
+        }
+    }
+}
